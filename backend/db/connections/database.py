bc7d1e13cf115698881154a8d7aa7cecbd62c0bf
lmfaolmaolol <vangsur68@gmail.com>
2025-03-22 05:15:44 +0530

    fixed some issues in db connection

--- backend/db/connections/database.py
+++ backend/db/connections/database.py
@@ -1,13 +1,14 @@
 
 from ..config import settings
 from typing import AsyncGenerator
-from sqlalchemy.ext.asyncio import create_async_engine, async_sessionmaker, AsyncSession
+from sqlalchemy.ext.asyncio import create_async_engine, AsyncSession
+from sqlalchemy.orm import sessionmaker
 
 # Create the async engine (ensure your DATABASE_URL is in the correct async format)
 engine = create_async_engine(settings.database_url_with_ssl, echo=True)
 
 # Configure the async session maker
-SessionLocal = async_sessionmaker(bind=engine, expire_on_commit=False)
+SessionLocal = sessionmaker(bind=engine, expire_on_commit=False, class_=AsyncSession)
 
 async def get_db() -> AsyncGenerator[AsyncSession, None]:
     async with SessionLocal() as session:
