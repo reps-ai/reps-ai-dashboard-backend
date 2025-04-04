dc7e715913dc28c4b307cbe1b5cf4fde0ab16f55
Aditya Bhaskara <62494443+adityabhas22@users.noreply.github.com>
2025-04-04 13:24:40 +0530

    fix the routes by removing tags

--- app/routes/__init__.py
+++ app/routes/__init__.py
@@ -2,4 +2,7 @@
 API Router modules
 """
 
-from app.routes import auth, leads, calls
+# Just expose the modules without importing them again
+from app.routes import auth
+from app.routes import leads
+from app.routes import calls
